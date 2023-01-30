fa7a1fac47f977f922c30ad64d0751cf3da1e0c5
alenakochetkova00 <alena-kochetkova00@mail.ru>
2023-01-30 22:21:07 +0300

    Successful data transfer via api and their further output

--- Convert/Currency.swift
+++ Convert/Currency.swift
@@ -1,10 +1,8 @@
 
 import Foundation
 
-struct CurrencyConversion: Codable {
-    let success: Bool
-    let terms, privacy: String
-    let timestamp: Int
-    let source: String
-    let quotes: [String: Double]
+
+// MARK: - Requested data structure
+struct CurrencyConversion: Decodable {
+    let rates: [String: Double]
 }
--- Convert/ViewController.swift
+++ Convert/ViewController.swift
@@ -1,40 +1,51 @@
 
 import UIKit
 
+// MARK: - View View Controller (working with api)
 class ViewController: UIViewController {
     
-    @IBOutlet var textLabel: UILabel!
-    let url = URL(string: "http://apilayer.net/api/live?access_key=b2ce674576c2bad12b1d07b0f708c485&currencies=RUB,%20AUD,%20AZN,%20GBP,%20AMD,%20BYN&source=USD&format=1")!
-
+    var arrayKeys = [String]()
+    var arrayValues = [Double]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        makeRequest()
+        fetchingApiData(URL: "https://open.er-api.com/v6/latest/USD") { result in
+            print(result)
         }
-    
-    func makeRequest() {
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print(error?.localizedDescription as Any)
-                return
-                
-            }
+    }
+
+    func fetchingApiData(URL url:String, completion: @escaping (CurrencyConversion) -> Void ) {
+        
+        let url = URL(string: url)
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: url!) { data, response, error in
+            if data != nil && error == nil {
                 do {
-                    let parsedData = try JSONDecoder().decode(CurrencyConversion.self, from: data)
-                    self.doLableChange(currency: parsedData)
+                    let parsingData = try JSONDecoder().decode(CurrencyConversion.self, from: data!)
+                    self.arrayKeys.append(contentsOf: parsingData.rates.keys)
+                    self.arrayValues.append(contentsOf: parsingData.rates.values)
                 } catch {
                     print("error")
-                    return
                 }
             }
-        task.resume()
         }
+        dataTask.resume()
+    }
+}
+
+
+//MARK: - Table View Controller (value output)
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        arrayKeys.count
+    }
     
-    func doLableChange(currency: CurrencyConversion) {
-        DispatchQueue.main.async {
-            self.textLabel.text = String(currency.quotes["USDRUB"]!)
-        }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = arrayKeys[indexPath.row]
+        cell?.detailTextLabel?.text = String(arrayValues[indexPath.row])
+        return cell!
     }
 }
 
