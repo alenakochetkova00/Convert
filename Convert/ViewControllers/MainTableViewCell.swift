7be0a94863bed24b6db6a1d59916760df3b9ad15
alenakochetkova00 <alena-kochetkova00@mail.ru>
2023-02-09 12:36:14 +0300

    displays data correctly

--- Convert/ViewControllers/MainTableViewCell.swift
+++ Convert/ViewControllers/MainTableViewCell.swift
@@ -2,21 +2,39 @@
 import UIKit
 
 class MainTableViewCell: UITableViewCell {
-
-    @IBOutlet var flag: UILabel!
+    
+    @IBOutlet var flagCountries: UILabel!
     @IBOutlet var nameCountries: UILabel!
     @IBOutlet var codeCountries: UILabel!
-    @IBOutlet var currencyValue: UILabel!
+    @IBOutlet var currencyCountries: UITextField!
+    
+    var codeMeaning = [String]()
+    var currencyMeaning = [Double]()
+    
+    var flag: String = ""
+    var name: String = ""
+    var code: String = ""
+    var currency: Double = 0.0
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        dataStorage()
+//        addDoneButtonOnKeyboard()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    func dataStorage() {
+        if UserDefaults.standard.object(forKey: "code") != nil {
+            codeMeaning = UserDefaults.standard.object(forKey: "code") as? [String] ?? []
+        }
+        if UserDefaults.standard.object(forKey: "currency") != nil {
+            currencyMeaning = UserDefaults.standard.object(forKey: "currency") as? [Double] ?? []
+        }
+    }
 }
--- Convert/ViewControllers/MainViewController.swift
+++ Convert/ViewControllers/MainViewController.swift
@@ -4,8 +4,6 @@ import UIKit
 class MainViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var editButton: UIBarButtonItem!
-    @IBOutlet var currencyText: UILabel!
     
     var listCurrency = [String : Double]()
     var listCurrencyName = [String]()
@@ -35,7 +33,7 @@ class MainViewController: UIViewController {
         }
         
         dataStorage()
-        receivingData()
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,77 +44,16 @@ class MainViewController: UIViewController {
     
     func dataStorage() {
         if UserDefaults.standard.object(forKey: "flag") != nil {
-            flag = UserDefaults.standard.object(forKey: "flag") as! [String]
+            flag = UserDefaults.standard.object(forKey: "flag") as? [String] ?? []
         }
         if UserDefaults.standard.object(forKey: "name") != nil {
-            name = UserDefaults.standard.object(forKey: "name") as! [String]
+            name = UserDefaults.standard.object(forKey: "name") as? [String] ?? []
         }
         if UserDefaults.standard.object(forKey: "code") != nil {
-            codes = UserDefaults.standard.object(forKey: "code") as! [String]
+            codes = UserDefaults.standard.object(forKey: "code") as? [String] ?? []
         }
-    }
-    
-    func receivingData() {
-        
-        let url = URL(string: "https://open.er-api.com/v6/latest/USD")
-        
-        let session = URLSession.shared
-        
-        let task = session.dataTask(with: url!) { data, response, error in
-            
-            if error != nil {
-                print(error?.localizedDescription as Any)
-                
-            } else {
-                
-                if data != nil {
-                    
-                    do {
-                        let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Dictionary<String, Any>
-                        
-                        print(jsonResponse)
-                        
-                        DispatchQueue.main.async { [self] in
-                            
-                            let array = Array(jsonResponse["rates"] as! [String : Double])
-                            
-                            for cod in codes {
-                            for (key, value) in array {
-                                
-                                var entity = CurrencyConversion()
-                                entity.code = key
-                                entity.value = value
-                                
-                                if cod == key {
-                                    self.listCurrencyName.append(key)
-                                    self.currency.append(value)
-                                
-                                    }
-                                }
-                            }
-                            
-                            print(currency)
-                            print(codes)
-                            print(listCurrencyName)
-                            self.tableView.reloadData()
-                        }
-                    } catch {
-                        print("error")
-                    }
-                }
-            }
-        }
-        task.resume()
-    }
-    
-    
-    @IBAction func editButtonClick(_ sender: Any) {
-        
-        tableView.isEditing = !tableView.isEditing
-        if tableView.isEditing {
-            editButton.title = "Ok"
-        } else {
-            editButton.title = "Edit"
+        if UserDefaults.standard.object(forKey: "currency") != nil {
+            currency = UserDefaults.standard.object(forKey: "currency") as? [Double] ?? []
         }
     }
 }
@@ -129,16 +66,16 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return currency.count
+        return flag.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellOnMainScreen", for: indexPath) as! MainTableViewCell
         
-        cell.flag.text = flag[indexPath.row]
+        cell.flagCountries.text = flag[indexPath.row]
         cell.nameCountries.text = name[indexPath.row]
         cell.codeCountries.text = codes[indexPath.row]
-        cell.currencyValue.text = String(currency[indexPath.row])
+        cell.currencyCountries.text = String(currency[indexPath.row])
        
         return cell
     }
@@ -147,48 +84,30 @@ extension MainViewController: UITableViewDataSource, UITableViewDelegate {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        
-        let selectedItemFlag = flag[sourceIndexPath.row]
-        flag.remove(at: sourceIndexPath.row)
-        flag.insert(selectedItemFlag, at: destinationIndexPath.row)
-        
-        let selectedItemName = name[sourceIndexPath.row]
-        name.remove(at: sourceIndexPath.row)
-        name.insert(selectedItemName, at: destinationIndexPath.row)
-        
-        let selectedItemCode = codes[sourceIndexPath.row]
-        codes.remove(at: sourceIndexPath.row)
-        codes.insert(selectedItemCode, at: destinationIndexPath.row)
-        
-        let selectedItemCurrency = currency[sourceIndexPath.row]
-        currency.remove(at: sourceIndexPath.row)
-        currency.insert(selectedItemCurrency, at: destinationIndexPath.row)
-        
-        UserDefaults.standard.setValue(flag, forKey: "flag")
-        UserDefaults.standard.setValue(name, forKey: "name")
-        UserDefaults.standard.setValue(codes, forKey: "code")
-        UserDefaults.standard.setValue(currency, forKey: "currency")
+    
+    // MARK: - Removing Data from a TableView
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         
-        tableView.beginUpdates()
-        flag.remove(at: indexPath.row)
-        name.remove(at: indexPath.row)
-        codes.remove(at: indexPath.row)
-        currency.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .fade)
-        tableView.endUpdates()
-        
-        UserDefaults.standard.setValue(flag, forKey: "flag")
-        UserDefaults.standard.setValue(name, forKey: "name")
-        UserDefaults.standard.setValue(codes, forKey: "code")
-        UserDefaults.standard.setValue(currency, forKey: "currency")
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            
+            flag.remove(at: indexPath.row)
+            name.remove(at: indexPath.row)
+            codes.remove(at: indexPath.row)
+            currency.remove(at: indexPath.row)
+            
+            UserDefaults.standard.setValue(flag, forKey: "flag")
+            UserDefaults.standard.setValue(name, forKey: "name")
+            UserDefaults.standard.setValue(codes, forKey: "code")
+            UserDefaults.standard.setValue(currency, forKey: "currency")
+            
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+            
+        }
     }
-
 }
